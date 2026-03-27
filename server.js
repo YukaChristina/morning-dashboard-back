6503a9ff2791c37c413351135b0e88a33f8dd733
Yuka <yukachristina@msn.com>
2026-03-27 19:24:32 +0900

    allow vercel origin in CORS

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import calendarRouter from './routes/calendar.js';
 dotenv.config();
 const app = express();
 
-app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:3000'] }));
+app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:3000', 'https://frontend-smoky-chi-72.vercel.app'] }));
 app.use(express.json());
 
 app.use('/api/markets', marketsRouter);
